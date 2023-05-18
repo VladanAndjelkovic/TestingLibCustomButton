643973d40ab98113de0086e979ad62630d4a12a5
Vladan  Andjelkovic <vladan.andjelkovic@alterset.net>
2023-05-18 19:57:10 +0200

    customButton2

---
+++ Example/Pods/Target Support Files/TestingLibCustomButton/TestingLibCustomButton-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_TestingLibCustomButton : NSObject
+@end
+@implementation PodsDummy_TestingLibCustomButton
+@end
--- TestingLibCustomButton/Classes/CustomButton2/CustomButton2.swift
+++ TestingLibCustomButton/Classes/CustomButton2/CustomButton2.swift
@@ -5,4 +5,75 @@
 //  Created by Vladan  Andjelkovic on 18.5.23..
 //
 
-import Foundation
+import UIKit
+
+public class CustomButton2: UIButton {
+    
+    public var action22: (() -> Void)?
+
+    public var fontColor: UIColor = .black {
+        didSet {
+            setTitleColor(fontColor, for: .normal)
+        }
+    }
+    
+    var btnBackgroundColor: UIColor = .yellow {
+        didSet {
+            self.backgroundColor = btnBackgroundColor
+        }
+    }
+    
+    var cornerRadius: CGFloat = 16 {
+        didSet {
+//            self.cornerRadius = cornerRadius
+            self.layer.cornerRadius = cornerRadius
+        }
+    }
+    
+    public convenience init (title: String?,
+                             fontColor: UIColor,
+                             fontSize: CGFloat = 25,
+                             btnBackgroundColor: UIColor,
+                             cornerRadius: CGFloat,
+                             superview: UIView) {
+        self.init(frame: .zero)
+        
+        setTitle(title, for: .normal)
+        
+        setTitleColor(fontColor, for: .normal)
+        
+        self.backgroundColor = btnBackgroundColor
+        
+        self.layer.cornerRadius = cornerRadius
+        
+        self.translatesAutoresizingMaskIntoConstraints = false
+        
+        superview.addSubview(self)
+        
+        NSLayoutConstraint.activate([
+            self.widthAnchor.constraint(equalTo: superview.widthAnchor, multiplier: 0.95),
+            self.heightAnchor.constraint(equalToConstant: 50),
+            self.centerXAnchor.constraint(equalTo: superview.centerXAnchor),
+            self.centerYAnchor.constraint(equalTo: superview.centerYAnchor)
+        ])
+        
+        self.addTarget(self, action: #selector(buttonAction), for: .touchUpInside)
+    }
+    
+    @objc
+    private func buttonAction() {
+        action22?()
+    }
+    
+    public override init(frame: CGRect) {
+//        self.fontColor = .clear
+        super.init(frame: frame)
+        self.fontColor = .clear
+
+    }
+    
+    public required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+}
+
